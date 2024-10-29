787f781eb1f7336a0a5d1436a302cda0956c4e33
Bruno Nascimento <brunonascimentodev@gmail.com>
2024-10-29 15:04:40 -0300

    change ip to localhost

--- modules/bor/default.nix
+++ modules/bor/default.nix
@@ -29,7 +29,7 @@ let
       grpc = {
         address = lib.mkOption {
           type = lib.types.str;
-          default = "127.0.0.1:3131";
+          default = "http://localhost:3131";
           description = "Address for the GRPC API.";
         };
       };
