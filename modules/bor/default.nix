84b826a5fb1788e41c5240e4baadb439d84ac49b
Bruno Nascimento <brunonascimentodev@gmail.com>
2024-11-08 12:01:00 -0300

    fix module

--- modules/bor/default.nix
+++ modules/bor/default.nix
@@ -72,7 +72,7 @@ in {
   ###### Interface
   options = {
     services.bor = lib.mkOption {
-      type = (lib.types.submodule borOpts);
+      type = (lib.types.attrsOf (lib.types.submodule borOpts));
       default = {};
       description = "Specification of one or more Bor node instances.";
     };
